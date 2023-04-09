5105c6f8eae2b832c3708a6c8945cb9a3ac0b2e4
Irham Maulana <i.maulana.azn@gmail.com>
2023-04-09 13:44:51 +0700

    make a config for graphql

--- src/main.js
+++ src/main.js
@@ -6,10 +6,28 @@ import router from './router'
 import './assets/main.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+
+const apolloClient = new ApolloClient({
+  uri: 'https://geodb-cities-graphql.p.rapidapi.com/',
+  headers: {
+    'X-RapidAPI-Key': 'edcc300743mshcbf51717b28b971p189912jsnc40500853b8d',
+    'X-RapidAPI-Host': 'geodb-cities-graphql.p.rapidapi.com'
+  },
+  cache: new InMemoryCache()
+})
 
 const app = createApp(App)
+app.provide(DefaultApolloClient, apolloClient)
 
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
+
+app.use(apolloProvider)
 app.use(createPinia())
 app.use(router)
-
+  
 app.mount('#app')
